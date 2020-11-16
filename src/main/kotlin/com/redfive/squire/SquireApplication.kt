e574e0b344049c1229a6a52624e9b9c735eed14f
Marc Miller <marc.miller.4@us.af.mil>
2020-11-16 17:50:55 -0500

    add initial rest api (to replace later with graphql)

--- src/main/kotlin/com/redfive/squire/SquireApplication.kt
+++ src/main/kotlin/com/redfive/squire/SquireApplication.kt
@@ -2,10 +2,25 @@ package com.redfive.squire
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.RequestParam
 
 @SpringBootApplication
 class SquireApplication
 
 fun main(args: Array<String>) {
     runApplication<SquireApplication>(*args)
+}
+
+@RestController
+class HelloController {
+
+  @GetMapping
+  fun helloWorld(@RequestParam(name = "name", defaultValue = "") name: String): String {
+    if (name != "") {
+      return "Hi, ${name}."
+    }
+    return "Hello world!"
+  }
 }
