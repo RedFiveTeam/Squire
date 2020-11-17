1758507caaff0bff4d435d7e9fdf41e0cd590b10
Marc Miller <marc.miller.4@us.af.mil>
2020-11-17 19:39:45 +0000

    update Jenkinsfile, add logback-spring/logback-test configurations, add application-test.yml, add initial test script, implement initial graphql query (no test yet)

---
+++ src/test/kotlin/com/redfive/squire/SquireApplicationTest.kt
@@ -0,0 +1,16 @@
+package com.redfive.squire
+
+import com.graphql.spring.boot.test.GraphQLTest
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+@GraphQLTest
+class SquireApplicationTest {
+
+    @Test
+    fun contextLoads() {
+    // This test just makes sure that the application will run, it doesn't test anything else.
+    }
+
+}
